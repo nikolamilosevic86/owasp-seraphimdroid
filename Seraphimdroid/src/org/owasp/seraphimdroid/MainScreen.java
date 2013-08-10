1e482f922c315b17ac9611970a1a517348e0dff5
nikola.milosevic <nikola.milosevic@prelovac.com>
2013-08-10 11:53:00 +0200

    initial commit

---
+++ Seraphimdroid/src/org/owasp/seraphimdroid/MainScreen.java
@@ -0,0 +1,22 @@
+package org.owasp.seraphimdroid;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainScreen extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_screen);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_screen, menu);
+		return true;
+	}
+
+}
