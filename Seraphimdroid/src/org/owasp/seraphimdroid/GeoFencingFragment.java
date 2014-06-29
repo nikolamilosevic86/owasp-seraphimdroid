f89dbbffb583d3e0d337ef080b243bd9b5563535
Furquan Ahmed <fur.ahmed@gmail.com>
2014-06-29 11:15:45 +0530

    Geolocation Map added

--- Seraphimdroid/src/org/owasp/seraphimdroid/GeoFencingFragment.java
+++ Seraphimdroid/src/org/owasp/seraphimdroid/GeoFencingFragment.java
@@ -1,19 +1,27 @@
 package org.owasp.seraphimdroid;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+
 public class GeoFencingFragment extends Fragment {
+
+	private GoogleMap googleMap;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_geofencing, container,
 				false);
+
+		googleMap = ((MapFragment) getActivity().getFragmentManager()
+				.findFragmentById(R.id.maps)).getMap();
+
 		return view;
 	}
 
