5be8cd9068b1672ed8f165bb262fbb99074d1f46
Aditya <addiittya2006@gmail.com>
2016-06-01 17:07:51 +0530

    minor fix redirect to app

--- Seraphimdroid/app/src/main/java/org/owasp/seraphimdroid/WiFiInfoActivity.java
+++ Seraphimdroid/app/src/main/java/org/owasp/seraphimdroid/WiFiInfoActivity.java
@@ -1,10 +1,7 @@
 package org.owasp.seraphimdroid;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class WiFiInfoActivity extends Activity {
 
@@ -15,24 +12,25 @@ public class WiFiInfoActivity extends Activity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_wi_fi_info, menu);
-        return true;
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu_wi_fi_info, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 }
